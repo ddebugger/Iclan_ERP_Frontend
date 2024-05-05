3c8f29d7f3958aceadde8a94fa90a93e3385bd36
ajong-trevor <trevor.ajong@gmail.com>
2024-05-05 22:11:06 +0100

    done with projects dashboard page

--- dist/js/dasboard.js
+++ dist/js/dasboard.js
@@ -1,9 +1,6 @@
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
 
-let menu = "<svg id='btn' width='22' height='22' viewBox='0 0 22 22' fill='none' xmlns='http://www.w3.org/2000/svg'> <path d='M4.01221 5.75586H18.2368M4.01221 11.0901H18.2368M4.01221 16.4243H18.2368' stroke='#545F71' stroke-width='1.77808' stroke-linecap='round' stroke-linejoin='round'/> </svg>";
-let closeMenu = "<svg id='btn' width='23' height='15' viewBox='0 0 23 15' fill='none' xmlns='http://www.w3.org/2000/svg'> <rect x='0.599609' y='0.5' width='21.6633' height='2' fill='#004F71'/> <rect x='5.17383' y='6.5' width='17.0916' height='2' fill='#004F71'/> <rect x='10.9414' y='12.5' width='11.3233' height='2' fill='#004F71'/> </svg>"
-
 closeBtn.addEventListener("click", () => {
   sidebar.classList.toggle("open");
   menuBtnChange();//calling the function(optional)
@@ -40,4 +37,32 @@ function openTab(evt, tabTitle) {
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(tabTitle).style.display = "block";
   evt.currentTarget.className += " active";
+}
+
+function checkTask(task) {
+  let checkbox1 = document.querySelector("#check-task1");
+  let checkbox2 = document.querySelector("#check-task2");
+  let checkbox3 = document.querySelector("#check-task3");
+
+  if (task === 'task1') {
+    if (checkbox1.checked) {
+      document.getElementById(task).className += "line-through";
+    } else {
+      document.getElementById(task).className = "";
+    }
+  } else if (task === 'task2') {
+    if (checkbox2.checked) {
+      document.getElementById(task).className += "line-through";
+    } else {
+      document.getElementById(task).className = "";
+    }
+  } else if (task === 'task3') {
+    if (checkbox3.checked) {
+      document.getElementById(task).className += "line-through";
+    } else {
+      document.getElementById(task).className = "";
+    }
+  }
+
+
 }
