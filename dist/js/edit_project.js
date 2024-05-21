06e0b021e6037ea2854f4e944e8cd22ff39c45fb
ajong-trevor <trevor.ajong@gmail.com>
2024-05-21 23:35:08 +0100

    join all pages in projects

--- dist/js/edit_project.js
+++ dist/js/edit_project.js
@@ -24,27 +24,27 @@ for (var i = 0; i < navItem.length; i++) {
   });
 }
 
-function openTab(event, tabName) {
-  var i, tabcontent, tablinks;
+// function openTab(event, tabName) {
+//   var i, tabcontent, tablinks;
 
-  // Hide all tab content
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+//   // Hide all tab content
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
 
-  // Remove the "active" class from all tab buttons
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active-item", "");
-  }
+//   // Remove the "active" class from all tab buttons
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active-item", "");
+//   }
 
-  // Show the selected tab content
-  document.getElementById(tabName).style.display = "grid";
+//   // Show the selected tab content
+//   document.getElementById(tabName).style.display = "grid";
 
-  // Add the "active" class to the button that opened the tab
-  event.currentTarget.className += " active-item";
-}
+//   // Add the "active" class to the button that opened the tab
+//   event.currentTarget.className += " active-item";
+// }
 
 // javascript for drag and drop a board element
 
