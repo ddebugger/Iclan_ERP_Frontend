995cdcd05a7411af90db3db5e103fd9164084dda
De La Cruz <delac1630@gmail.com>
2024-08-02 20:07:32 -0700

    modals
    pop up modals

---
+++ dist/js/project_modals.js
@@ -0,0 +1,5 @@
+
+// Create New Project Modal
+// const openModalButton = document.getElementById("project-modal-btn");
+// const closeModalButton = document.getElementById("close-project-modal");
+// const modal = document.getElementById("project-modal");
